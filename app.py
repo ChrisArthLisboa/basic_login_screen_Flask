8e9af708aa404df9622fe3fa2f26f68fa75e2f49
ChrisArthLisboa <19lisboa19@gmail.com>
2024-02-23 21:55:01 -0300

    Register/login mode +database start

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import flask
+import mysql.connector
 
 app = flask.Flask(__name__)
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database=""
+)
+cursor = db.cursor()
 
 
 # Renders: 
@@ -13,20 +21,38 @@ def index():
 def login():
 
     def valid_login(name, password):
-        if name == "admin" and password == "12345":
-            return True
+        if name in cursor.execute("Select name in table"):
+            if password == cursor.execute(f"Select password in table where name = {name}"):
+                return True
+            else:
+                return False
         else:
             return False
 
     def log_user(name):
-        return flask.render_template("login.html", login=name)
+        return flask.render_template("index.html", login=True, username=name)
     
+    def existAccount(username, email):
+        account = cursor.execute(f"Select username, email where username={username}")
+            
+
+
+    def createAccount(username, email, password):
+        pass
 
     myError = ""
     if flask.request.method == "POST":
+        
         if valid_login(flask.request.form['name'], flask.request.form['password']):
             return log_user(flask.request.form['name'])
         else:
             myError = "Invalid name/password"
+
+        if existAccount(flask.request.form['username_register'], flask.request.form['email_register']):
+            createAccount(flask.request.form['username_register'], flask.request.form['email_register'], flask.request.form['password_register'])
+            log_user(flask.request.form['username_register'])
+        else:
+            myError = "name already taken/ email already exists"
+
 
     return flask.render_template("login.html", error=myError)
